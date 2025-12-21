e6536d4d551eb1d1ebff457c52f4b23118d718ec
JordyBacherot <jordy.bacherot@outlook.fr>
2025-12-21 11:00:11 +0100

    Remove sitemap plugin configuration from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // 2. Configuration du sitemap
-    Sitemap({
-      hostname: 'https://jordy-bacherot.vercel.app',
-    }),
   ],
   resolve: {
     alias: {
