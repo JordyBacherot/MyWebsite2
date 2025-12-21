7713bf140816a21d54933888f5f59df21bf0e6c3
JordyBacherot <jordy.bacherot@outlook.fr>
2025-12-21 10:17:01 +0100

    feat: Add and configure `vite-plugin-sitemap` for sitemap generation.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import Sitemap from 'vite-plugin-sitemap'
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 2. Configuration du sitemap
+    Sitemap({
+      hostname: 'https://jordy-bacherot.vercel.app',
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
